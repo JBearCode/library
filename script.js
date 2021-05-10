b7912e731f060607393d81ca4482a2164aed1257
JBearCode <joelbmoran@gmail.com>
2021-05-10 11:28:33 -0400

    Add updateTable()

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ const form = document.getElementById("mainform");
 form.addEventListener("submit", addBookToLibrary)
 
 let myLibrary = [];
+let idCounter = 0;
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, id) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+  this.id = id;
 }
 
 function addBookToLibrary(e) {
@@ -18,6 +20,23 @@ function addBookToLibrary(e) {
   const author = document.getElementById('author').value;
   const pages = document.getElementById('pages').value;
   const alreadyRead = document.getElementById('alreadyread').checked;
+  myLibrary.push(new Book(title, author, pages, alreadyRead, idCounter++));
   console.log("addBookToLibrary ran");
-  myLibrary.push(new Book(title, author, pages, alreadyRead));
 }
+
+function updateTable() {
+  const bookTable = document.getElementById('booktable');
+
+  for (let i = 0; i < myLibrary.length; i++) {
+    bookTable += "<tr>"
+    bookTable += "<td>" + myLibrary[i].title + "</td>"
+    bookTable += "<td>" + myLibrary[i].author + "</td>"
+    bookTable += "<td>" + myLibrary[i].pages + "</td>"
+    bookTable += "<td>" + myLibrary[i].read + "</td>"
+    bookTable += "<td>🗑️</td>"
+    bookTable += "</tr>"
+  }
+
+
+}
+
